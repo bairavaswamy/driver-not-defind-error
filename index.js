8070454d9dce1a4eaf6513153b0cb5002d9bd99f
bairavaswamy <bairavahoney@gmail.com>
2023-12-21 07:48:30 +0000

    error

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const sqlite3 = require('sqlite3')
+const {open} = require('sqlite')
+
+const app = express()
+const path = require('path')
+const dbpath = path.join(__dirname, 'goodreads.db')
+let db = null
+const initiatingServerDBandNode = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqlite3.Datebase,
+    })
+    app.listen(3000, () => {
+      console.log('Server running...')
+    })
+  } catch (error) {
+    console.log(`Datebase Error ${error.message}`)
+    process.exit(1)
+  }
+}
+
+initiatingServerDBandNode()
